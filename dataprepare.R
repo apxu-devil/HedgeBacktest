28f2594f56df6679bfcb6528c34ee43536985034
Andrey Arhipov <arh.andr@gmail.com>
2016-08-07 18:07:58 +0300

    swap fixed

--- dataprepare.R
+++ dataprepare.R
@@ -15,7 +15,7 @@ options(scipen = 999)
 allUSDRUB = NULL
 foreach (i = 1:15) %do% {
   
-  getFX('USD/RUB', from = paste(2000 + i, '-01-01', sep=''), to = paste(2000 + i + 1, '-01-01', sep=''),)
+  getFX('USD/RUB', from = paste0(2000 + i, '-01-01'), to = paste0(2000 + i + 1, '-01-01'))
   if(is.null(allUSDRUB)) allUSDRUB=USDRUB else allUSDRUB=c(allUSDRUB, USDRUB)
 }
 
@@ -23,19 +23,12 @@ rm(USDRUB)
 allUSDRUB = allUSDRUB[which(ROC(allUSDRUB$USD.RUB)!=0), ]
 
 
-#
-# Get historical IV
-# 
 
-iv = read.csv('usdrub_iv.csv', sep=';') 
-iv = xts(x = iv[,2:4], order.by = as.Date(as.character(iv[,1]), format='%d.%m.%Y'))
-names(iv) = c('iv1m', 'iv3m', 'iv1y')
 
 #
 # Get data and split swaps data
 # 
 
-### TODO: Change data source to files
 
 swap3m = read.csv(file='swap3m.csv', sep = ';')
 names(swap3m) = c('Date', 'swap3m')
@@ -49,16 +42,26 @@ swap1y$Date = as.Date(swap1y$Date, '%d.%m.%Y')
 swaps = full_join(swap1y, swap3m, by = 'Date')
 swaps = as.xts(x = swaps[,2:3,drop=F], order.by = swaps$Date) %>% na.locf
 
+# temp
+#allUSDRUB = rub3m$rub
+
 # Merge rub and swap data
-rubswap = cbind.xts(allUSDRUB, swaps, all = c(F,T)) %>% na.locf %>% na.omit
+rub = cbind.xts(allUSDRUB, swaps, all = c(F,T)) %>% na.locf %>% na.omit
 
-rubmrtk = cbind.xts(rubswap, iv, all = c(T,T)) %>% na.omit
+# Swaps in % annual
 
-# Calc swaps in % annual
-rubmrtk$swap3m_perc = (as.data.frame(rubmrtk) %>% mutate(swp3 = swap3m/1000000/USD.RUB*90) %>% select(swp3))[[1]]
-rubmrtk$swap1y_perc = (as.data.frame(rubmrtk) %>% mutate(swp1 = swap1y/1000000/USD.RUB*365) %>% select(swp1))[[1]]
+rub$swap3m = (as.data.frame(rub) %>% mutate(swap3m = swap3m/10000/rub/90*365))$swap3m
+rub$swap1y = (as.data.frame(rub) %>% mutate(swap1y = swap1y/10000/rub))$swap1y
+#
+# Get historical IV
+# 
+
+iv = read.csv('usdrub_iv.csv', sep=';') 
+iv = xts(x = iv[,2:4]/100, order.by = as.Date(as.character(iv[,1]), format='%d.%m.%Y'))
+names(iv) = c('iv1m', 'iv3m', 'iv1y')
+rub = cbind.xts(rub, iv, all = c(T,T)) %>% na.omit
 
 # Save data
-save(rubmrtk, file = 'rubswap.RData')
+save(rubmrtk, file = 'rub.RData')
 
 
